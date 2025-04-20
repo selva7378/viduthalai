93d890f2eebcbce2695d6fba8bc3484f613507bb
SELVA <selva@SELVAs-MacBook-Air.local>
2025-04-20 11:43:22 +0530

    implemented blockit features

--- app/src/main/java/com/example/viduthalai/MyDeviceAdminReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.example.viduthalai
-
-import android.app.admin.DeviceAdminReceiver
-
-// Device admin receiver (needs to be declared in AndroidManifest.xml)
-class MyDeviceAdminReceiver : DeviceAdminReceiver() {
-    // Can add custom behavior here if needed
-}
